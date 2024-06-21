0aecf5abdc1aac82d52925bd45713d358a522f60
aadriann12 <aterans03@educantabria.es>
2024-06-21 16:18:20 +0200

    poef

---
+++ mavenproject10/src/main/java/com/mycompany/mavenproject10/Mavenproject10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject10;
+
+/**
+ *
+ * @author DAW109
+ */
+public class Mavenproject10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello jimena!");
+    }
+}
